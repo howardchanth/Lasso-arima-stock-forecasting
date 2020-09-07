b6c083c020c2d399bf46b37c911e615f7b76392d
howardchanth <u3537635@connect.hku.hk>
2020-09-07 23:51:31 +0800

    Integrating models to Models.py

--- Forecaster.py
+++ Forecaster.py
@@ -1,3 +1,18 @@
+from Models import LassoForecaster, ArimaForecaster, GBMForecaster
+
 # Data paths
 DATA_PATH_SP = "data/SP500_aug_15to20.csv"
 DATA_PATH_HSI = "data/HSI_aug_15to20.csv"
+
+""" Model parameters"""
+PARAMS = {
+
+}
+
+# Start simulation
+forecaster = GBMForecaster(DATA_PATH_SP)
+forecaster.fit(1000)
+conf_int = forecaster.confidence_interval()
+forecaster.plot()
+
+print(conf_int[1][365], conf_int[0][365])
--- Models.py
+++ Models.py
@@ -3,6 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from abc import ABC, abstractmethod
 
+# Lasso
+from sklearn.model_selection import cross_val_score, GridSearchCV
+from sklearn.linear_model import Ridge, Lasso
+
+from statsmodels.graphics.tsaplots import plot_pacf
+from statsmodels.tsa.arima_model import ARIMA
+
 
 class Forecaster(ABC):
     """Abstract class for financial forecaster"""
@@ -29,7 +36,7 @@ class Forecaster(ABC):
         pass
 
 
-class GBM(Forecaster):
+class GBMForecaster(Forecaster):
     """
         Parameter Definitions
         Ref:
@@ -89,12 +96,13 @@ class GBM(Forecaster):
                                 for scene in range(1, scene_size + 1)])
         self.S_pred = np.insert(self.S_pred, 0, self.S_0, axis=1)
 
-    def predict(self):
+    def det_proj(self):
         """Deterministic projection"""
-        return np.mean(self.S_pred, axis=1)
+        return np.mean(self.S_pred, axis=0)
 
     def confidence_interval(self):
         # TODO: Need to double confirm whether naively sorting the array is appropriate
+        # TODO: Include a point projection
         sorted_price = np.sort(self.S_pred, axis=0)
 
         # Upper and lower limits
@@ -104,5 +112,69 @@ class GBM(Forecaster):
         return lower, upper
 
     def plot(self):
-        # TODO: Plot stock price projection
+        series_pred = self.det_proj()
+        days = range(len(series_pred))
+        lower, upper = self.confidence_interval()
+
+        fig, ax = plt.subplots()
+        ax.plot(days, series_pred)
+        ax.fill_between(days, lower, upper, color='b', alpha=.1)
+        plt.ylabel('Stock Prices')
+        plt.xlabel('Prediction Days')
+        plt.show()
+
+
+class LassoForecaster(Forecaster):
+
+    def __init__(self, data_path):
+        super().__init__(data_path)
+
+        # Grid Search parameters
+        self.parameters = {'alpha': [1e-15, 1e-10, 1e-8, 1e-4, 1e-3, 1e-2, 1, 5, 10, 20]}
+        self.predictors = self.raw_data.drop(['Adj Close', 'Date'], axis=1)
+        self.responses = self.raw_data['Adj Close'].values.reshape(-1, 1)
+        self.model = Lasso()
+        self.regressor = GridSearchCV(self.model, self.parameters, scoring='neg_mean_squared_error', cv=5)
+
+    def fit(self, *args, **kwargs):
+        self.regressor.fit(self.predictors, self.responses)
+
+    def confidence_interval(self):
+        pass
+
+
+# Date parser
+def parser(x):
+    return pd.datetime.strptime(x, '%Y-%m-%d')
+
+
+class ArimaForecaster(Forecaster):
+
+    def __init__(self, data_path):
+        super().__init__(data_path)
+        self.series = pd.read_csv(data_path, header=0, parse_dates=[0],
+                                  index_col=0, squeeze=True, date_parser=parser)
+        self.model = None  # Self-defined after evaluating the ACF and PACFs
+        self.model_fit = None
+
+    def plot_acf(self):
+        pd.plotting.autocorrelation_plot(self.series)
+        plt.show()
+
+    def plot_pacf(self):
+        plot_pacf(self.series['Adj Close'])
+        plt.show()
+
+    def fit(self, p, d, q):
+        self.model = ARIMA(self.series['Adj Close'], order=(p, d, q))
+        self.model_fit = self.model.fit(disp=0)
+        return self.model_fit
+
+    def confidence_interval(self):
+        pass
+
+    def summary(self):
+        return self.model_fit.summary()
+
+    def plot(self):
         pass
